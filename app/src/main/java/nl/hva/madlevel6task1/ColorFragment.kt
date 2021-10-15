f3b7c70e559e5eb32f51e54885657786f328c138
Silvan Wenk <silvan.wenk@stud.hslu.ch>
2021-10-15 10:00:26 +0200

    Empty ColorFragment

--- app/src/main/java/nl/hva/madlevel6task1/ColorFragment.kt
+++ app/src/main/java/nl/hva/madlevel6task1/ColorFragment.kt
@@ -5,18 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import nl.hva.madlevel6task1.databinding.FragmentSecondBinding
+import nl.hva.madlevel6task1.databinding.FragmentColorBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
-class SecondFragment : Fragment() {
+class ColorFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: FragmentColorBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -24,17 +17,13 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentColorBinding.inflate(inflater, container, false)
         return binding.root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
     }
 
     override fun onDestroyView() {
